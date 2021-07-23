c37d93839773345e5450ff3f9552a34307302c91
Dima Dorezyuk <dorezyuk@magazino.eu>
2021-07-23 14:09:45 +0200

    fix segmentation fault

--- src/sob_layer.cpp
+++ src/sob_layer.cpp
@@ -292,7 +292,7 @@ SobLayer::horizontalSwipe(Costmap2D& _master, int dist, int min_i, int min_j,
   for (auto jj = min_j; jj != max_j; ++jj) {
     // begin of the interesting memory chunk.
     const auto min_rr = _master.getIndex(0, jj);
-    const auto map_x_begin = map_x_.begin() + (min_rr + min_i);
+    auto map_x_begin = map_x_.begin() + (min_rr + min_i);
 
     // init vars
     v[0] = min_i;
@@ -301,7 +301,7 @@ SobLayer::horizontalSwipe(Costmap2D& _master, int dist, int min_i, int min_j,
     k = 0;
 
     {
-      const auto end = map_x_begin + max_i - min_i;
+      const auto end = map_x_.begin() + (min_rr + max_i);
 #pragma GCC ivdep
       for (auto ss = map_x_begin, dd = map_x_sq_.begin() + min_i; ss != end;
            ++ss, ++dd)
@@ -313,10 +313,10 @@ SobLayer::horizontalSwipe(Costmap2D& _master, int dist, int min_i, int min_j,
     if (map_x_sq_[min_i] >= sq_dist)
       map_x_sq_[min_i] = std::numeric_limits<int>::max();
 
-    for (int ii = min_i + 1; ii != max_i; ++ii) {
+    ++map_x_begin;
+    for (int ii = min_i + 1; ii != max_i; ++ii, ++map_x_begin) {
       // ignore everything we don't care about
-      const auto& ii_v = *(map_x_begin + ii);
-      if (ii_v >= dist)
+      if (*map_x_begin >= dist)
         continue;
 
       s = parabolaIntersection(ii, map_x_sq_[ii], v[k], map_x_sq_[v[k]]);
@@ -360,7 +360,7 @@ SobLayer::horizontalSwipe(Costmap2D& _master, int dist, int min_i, int min_j,
 
     for (; k != k_end; ++k) {
       // init the helpers
-      const auto row = static_cast<size_t>(*(map_x_begin + v[k]));
+      const auto row = static_cast<size_t>(map_x_[min_rr + v[k]]);
       const auto& cache_row = cache_.at(row);
       const auto cache_size = (int)cache_row.size();
 
